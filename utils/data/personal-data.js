9d966e8e5684688094b1d642984917d049029e6b
vierdev <jamiemmacl@outlook.com>
2025-05-28 01:06:44 -0700

    Initial commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,11 @@
+export const personalData = {
+  name: "Jamie Macleod",
+  profile: '/profile.png',
+  designation: "Fullstack LLM Engineer",
+  description: "My name is Jamie Macleod. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Python and Javascript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'macleodjamie195@gmail.com',
+  phone: '+17787830780',
+  address: '204-1353 1ST AVE E Vancouver BC V5N 1A1 ',
+  github: 'https://github.com/vierdev',
+  resume: "https://drive.google.com/file/d/18xiDccwfmovW8fe___i1Qi5am5N95lfM/view?usp=sharing"
+}
